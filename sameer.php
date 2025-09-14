3d4af7dd555180289661723a5dc1883164129ffe
sameer tariq <sameertariq882@gmail.com>
2025-09-14 14:57:37 +0500

    accessorires-done

---
+++ sameer.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fuild d-flex justify-content-center align-items-center">
+        <img  src="https://techhunk.pk/cdn/shop/files/essentialweb_11zon.webp?v=1757536727&width=2400" alt="" style="width: 100%; height: 50%;">
+    </div>
+    
+    <h1 class=" text-center fw-bold py-3"> <span class=" text-dark"> Tech</span>  <span style="color:#1F4DB3;"> Essentials</span></h1>
+    <div class="container d-flex justify-content-between flex-wrap py-3 gap-3">
+    
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+
+  
+
+</div>
+    <div class="container d-flex justify-content-between flex-wrap py-3 gap-3">
+    
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+
+  
+
+</div>
+    <div class="container d-flex justify-content-between flex-wrap py-3 gap-3">
+    
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+
+  
+
+</div>
+    <div class="container d-flex justify-content-between flex-wrap py-3 gap-3">
+    
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+  <div class="card border-0 rounded-4 shadow" style="width: 250px;">
+    <img class="w-100" src="https://techhunk.pk/cdn/shop/files/Untitled-1.webp?v=1757773643&width=352" alt="">
+    <h5 class="fw-semibold text-dark px-3">Apple Original 20W charger & Cable</h5>
+    <p class="text-dark px-3" style="font-size: 15px;">Apple Charger 20W</p>
+    <div class="py-2 "> <button class="btn btn text-white mx-auto d-block py-1 rounded-pill" style="background-color:#132240; width:85%;">Add to cart</button></div>
+  </div>
+
+  
+
+</div>
+
+
+
+<div class=" py-3">
+
+<div class="container-fuild d-block mx-auto text-white py-5 px-4" style="border-radius: 25px; background-color: #142343; width:95%; ">
+    <div class="row">
+        <!-- Shop Column -->
+        <div class="col-md-3 mb-4">
+            <h6 class="fw-bold">SHOP</h6>
+        <ul class="list-unstyled">
+          <li><a href="#" class=" text-decoration-none py-2 " style=" color:#94A0C5;">Earbuds</a></li>
+          <li><a href="#" class=" text-decoration-none py-2 " style=" color:#94A0C5;">Smart Watches</a></li>
+          <li><a href="#" class=" text-decoration-none py-2 " style=" color:#94A0C5;">Projectors</a></li>
+          <li><a href="#" class=" text-decoration-none py-2 " style=" color:#94A0C5;">Accessories</a></li>
+        </ul>
+      </div>
+
+      <!-- Help Column -->
+      <div class="col-md-3 mb-4">
+        <h6 class="fw-bold">HELP</h6>
+        <ul class="list-unstyled">
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Home Page</a></li>
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Support</a></li>
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Privacy Policy</a></li>
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Terms & Conditions</a></li>
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Shipping Policy</a></li>
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Warranty Claim</a></li>
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Warranty Policy</a></li>
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Track your Order</a></li>
+          <li><a href="#" class=" text-decoration-none" style=" color:#94A0C5;">Contact us</a></li>
+        </ul>
+      </div>
+
+      <!-- Contact Column -->
+      <div class="col-md-4   ">
+        <div class="d-flex flex-column align-items-md-end align-items-start">
+            <div class=" text-align-left">
+          <!-- Logo -->
+          <img src="https://techhunk.pk/cdn/shop/files/logo.png" alt="Tech Hunk" style="max-height: 40px;">
+
+          <!-- Social Icons -->
+          <div class="my-3 d-flex justify-content-between align-items-center gap-3">
+            <div class="bi bi-facebook"></div>
+            <div class="bi bi-instagram"></div>
+            <div class="bi bi-youtube"></div>
+          </div>
+
+          <!-- Contact Info -->
+          <p class="mb-1 fw-semibold">We’re here to help.</p>
+          <p class="mb-1">Call Us: +92 305 1114485</p>
+          <p class="mb-3">Email Us: info@techhunk.pk</p>
+          </div>
+
+          <!-- Newsletter -->
+          <p class="mb-2 fw-bold text-primary">Get exclusive offers and updates</p>
+          <form class="d-flex w-100" style="max-width: 400px;">
+            <input type="email" class="form-control rounded-4" placeholder="Email Address">
+            <button type="submit" class="btn btn-light rounded-4 ms-2 py-2 ">Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+   
+    
+
+
+
+
+
+    
+</body>
+</html>
