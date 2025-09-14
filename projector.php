9a3687cb4faa553231784eed147e2d5e6c01e65e
unknown <ch.fahad34250@gmail.com>
2025-09-14 14:56:06 -0700

    sdfs

---
+++ projector.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projector</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+   <style>
+    :root{
+        --blue:#0E1B4D;
+    }
+    .text-blue{
+        color:var(--blue);
+    }
+    nav{
+        background-color: rgba(255, 255, 255, 0.5);
+        border: 1px solid rgba(255, 255, 255, 0.28);
+    }
+    .discount{
+        background-color: #FFEEF0;
+        color: #E45050;
+        width: max-content;
+        font-size: 0.6rem;
+        
+    }
+    .fs-tiny{
+        font-size: 0.8rem;
+    }
+    .discount p{
+        margin: auto;
+    }
+    .main{
+        flex-wrap: wrap;
+    }
+    .final{
+        width: 97%;
+    }
+    .bg-blue{
+        background-color: #142343;
+    }
+    input{
+      width: 60%;
+    }
+   </style>
+</head>
+<body>
+    <div class="container-fluid min-vh-80 p-0 position-relative shadow">
+        
+        <nav class="container bg-transparent mt-2 d-flex justify-content-center align-item-center gap-5 position-absolute rounded-4" style="left: 50%; transform: translateX(-50%);backdrop-filter: blur(10px);">
+            <img src="https://techhunk.pk/cdn/shop/files/logo_1.png?v=1756293749&width=140" width="100px" height="30px" alt="" style="margin-top:0.5rem;">
+            <ul class="list-unstyled d-flex gap-5 align-items-center mt-3 text-white">
+                <li>Home</li>
+                <li>Earbuds</li>
+                <li>SmartWatches</li>
+                <li>Projectors</li>
+                <li>Accessories</li>
+                <li>Support</li>
+            </ul>
+            <div class="d-flex gap-3 align-items-center text-white">
+                <i class="bi fs-5 bi-search"></i>
+                <i class="bi fs-5 bi-person"></i>
+                <i class="bi fs-5 bi-bag"></i>
+            </div>
+
+
+        </nav>
+        
+        <img src="https://techhunk.pk/cdn/shop/files/projetor_11zon.webp?v=1757531250&width=2400" width="100%" height="100%" alt="">
+    </div>
+    <h2 class="text-center mt-5">TECHHUNK <span class="text-primary">PROJECTORS</span></h2>
+    <div class="main container d-flex gap-5 mt-5">
+        <div class="card shadow px-3 py-2 border-0" style="width: 21%;">
+            <img src="https://techhunk.pk/cdn/shop/files/2.webp?v=1757516714&width=352" width="100%" alt="">
+            <div class="discount rounded-pill px-2 py-1"><p>40% OFF</p></div>
+            <p class="fw-semibold">TH Portable Projector Gen 2</p>
+            <p>TH Smart Portable Projector</p>
+            <p></p>
+            <p><del class="text-secondary ">Rs.40,000 </del><span class="fw-bold">Rs.23,650</span></p>
+            <button class="btn btn-primary w-80 rounded-pill">Add To Cart</button>
+        </div>
+        <div class="card shadow px-3 py-2 border-0" style="width: 21%;">
+            <img src="https://techhunk.pk/cdn/shop/files/4webp.webp?v=1757516721&width=352" width="100%" alt="">
+            <div class="discount rounded-pill px-2 py-1"><p>40% OFF</p></div>
+            <p class="fw-semibold">TH Portable Projector Gen 2</p>
+            <p>TH Smart Portable Projector</p>
+            <p></p>
+            <p><del class="text-secondary ">Rs.40,000 </del><span class="fw-bold">Rs.23,650</span></p>
+            <button class="btn btn-primary w-80 rounded-pill">Add To Cart</button>
+        </div>
+        <div class="card shadow px-3 py-2 border-0" style="width: 21%;">
+            <img src="https://techhunk.pk/cdn/shop/files/1.webp?v=1757516708&width=352" width="100%" alt="">
+            <div class="discount rounded-pill px-2 py-1"><p>40% OFF</p></div>
+            <p class="fw-semibold">TH Portable Projector Gen 2</p>
+            <p>TH Smart Portable Projector</p>
+            <p></p>
+            <p><del class="text-secondary ">Rs.40,000 </del><span class="fw-bold">Rs.23,650</span></p>
+            <button class="btn btn-primary w-80 rounded-pill">Add To Cart</button>
+        </div>
+        <div class="card shadow px-3 py-2 border-0" style="width: 21%;">
+            <img src="https://techhunk.pk/cdn/shop/files/5webp.webp?v=1757516721&width=352" width="100%" alt="">
+            <div class="discount rounded-pill px-2 py-1"><p>40% OFF</p></div>
+            <p class="fw-semibold">TH Portable Projector Gen 2</p>
+            <p>TH Smart Portable Projector</p>
+            <p></p>
+            <p><del class="text-secondary ">Rs.40,000 </del><span class="fw-bold">Rs.23,650</span></p>
+            <button class="btn btn-primary w-80 rounded-pill">Add To Cart</button>
+        </div>
+        <div class="card shadow px-3 py-2 border-0" style="width: 21%;">
+            <img src="https://techhunk.pk/cdn/shop/files/gen3prjjctr.webp?v=1757373027&width=352" width="100%" alt="">
+            <div class="discount rounded-pill px-2 py-1"><p>40% OFF</p></div>
+            <p class="fw-semibold">TH Portable Projector Gen 2</p>
+            <p>TH Smart Portable Projector</p>
+            <p></p>
+            <p><del class="text-secondary ">Rs.40,000 </del><span class="fw-bold">Rs.23,650</span></p>
+            <button class="btn btn-primary w-80 rounded-pill">Add To Cart</button>
+        </div>
+    </div>
+    <div class="container">
+        <h2 class="text-blue fw-bold my-4 text-center">Frequently Asked Questions</h2>
+        <div class="accordion w-50 mx-auto" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button text-blue fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Accordion Item #1
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+      <div class="accordion-body text-blue fw-bold">
+        <strong>This is the first item’s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed text-blue fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Accordion Item #2
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body text-blue fw-bold">
+        <strong>This is the second item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed text-blue fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Accordion Item #3
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body text-blue fw-bold">
+        <strong>This is the third item’s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It’s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+    <div class="container">
+        <h2 class="fw-bold text-blue mt-5 mb-3">Projector Price in Pakistan | Smart & Portable – Tech Hunk</h2>
+        <p class="fs-5 text-secondary">Shop smart & portable projectors in Pakistan—1080p, Android, screen mirroring, keystone & low-latency game modes. Fast delivery, COD & local warranty.</p>
+        <p class="fs-5 text-secondary">Bring the <span class="fw-bold">big screen home</span> with <a href="#">techhunk projectors</a>— from <span class="fw-bold">portable Android projectors</span> for movie nights to smart models with screen...</p>
+        <a href="#" class="text-decoration-none text-primary fs-tiny fw-bold">Read More+</a>
+    </div>
+    <div class="container-fluid final bg-blue rounded-4 mt-5 mb-3">
+        <div class="row">
+            <div class="col-lg-2 ms-2 ">
+                <ul class="list-unstyled text-white mt-5">
+                    <li class="mt-2 fw-bold">SHOP</li>
+                    <li class="mt-2 text-secondary fw-semibold">Earbuds</li>
+                    <li class="mt-2 text-secondary fw-semibold">Smart Watches</li>
+                    <li class="mt-2 text-secondary fw-semibold">Projectors</li>
+                    <li class="mt-2 text-secondary fw-semibold">Accessories</li>
+                </ul>
+            </div>
+            <div class="col-lg-4 ">
+                <ul class="list-unstyled text-white mt-5 ">
+                    <li class="mt-2 fw-bold">HELP</li>
+                    <li class="mt-2 fw-semibold"><a href="#" class="text-decoration-none text-secondary">Home Page</a></li>
+                    <li class="mt-2 fw-semibold"><a href="#" class="text-decoration-none text-secondary">Support</a></li>
+                    <li class="mt-2 fw-semibold"><a href="#" class="text-decoration-none text-secondary">Privacy Policy</a></li>
+                    <li class="mt-2 fw-semibold"><a href="#" class="text-decoration-none text-secondary">Terms & Conditions</a></li>
+                    <li class="mt-2 fw-semibold"><a href="#" class="text-decoration-none text-secondary">Shipping Policy</a></li>
+                    <li class="mt-2 fw-semibold"><a href="#" class="text-decoration-none text-secondary">Warranty Claim</a></li>
+                    <li class="mt-2 fw-semibold"><a href="#" class="text-decoration-none text-secondary">Warranty Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-5 mt-4">
+                <img class="d-block" src="https://techhunk.pk/cdn/shop/files/logo_1.png?v=1756293749&width=320" width="120px" height="30px" alt="">
+                <div class="d-flex gap-4 ms-2 mt-3">
+                  <i class="bi text-white bi-facebook"></i>
+                  <i class="bi text-white bi-instagram"></i>
+                  <i class="bi text-white bi-youtube"></i>
+                </div>
+                <h5 class="text-white fw-bold mt-4 ">We’re here to help.</h5>
+                <p class="text-white fs-5 m-0 p-0">Call Us: +92 305 1114485</p>
+                <p class="text-white fs-5">Email Us: info@techhunk.pk</p>
+                <div class="d-flex gap-2 ">
+
+                  <input type="text" placeholder="Email Address" class="border-0 rounded-4 px-4 py-2">
+                  
+                  <button class="btn btn-white rounded-4 fw-semibold">Submit</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
